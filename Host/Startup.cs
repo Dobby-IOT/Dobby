4f2e29780fa4afbc271ec710717bdc4a54df3b9e
lucien.zimmermann <lucien.zimmermann@swisslife.ch>
2019-09-16 07:39:49 +0200

    Cleanup in Startup-class

--- Host/Startup.cs
+++ Host/Startup.cs
@@ -3,46 +3,37 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using HotChocolate.AspNetCore;
 using HotChocolate;
+using HotChocolate.Subscriptions;
+using HotChocolate.AspNetCore.Authorization;
 
 namespace Dobby.Host
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
-            // If you need dependency injection with your query object add your query type as a services.
-            // services.AddSingleton<Query>();
+            services.AddInMemorySubscriptionProvider();
 
-            // enable InMemory messaging services for subscription support.
-            // services.AddInMemorySubscriptionProvider();
-
-            // this enables you to use DataLoader in your resolvers.
             services.AddDataLoaderRegistry();
 
             // Add GraphQL Services
             services.AddGraphQL(sp => SchemaBuilder.New()
-                // enable for authorization support
-                // .AddDirectiveType<AuthorizeDirectiveType>()
+                .AddDirectiveType<AuthorizeDirectiveType>()
                 .AddQueryType<Query>()
                 .Create());
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UsePlayground();
             }
 
-            // enable this if you want tu support subscription.
-            // app.UseWebSockets();
+            app.UseWebSockets();
             app.UseGraphQL();
-            // enable this if you want to use graphiql instead of playground.
-            // app.UseGraphiQL();
-            app.UsePlayground();
         }
     }
 }
